a30e2b256338693953b03ee555d86776e7bdd452
salamander-mh <1906747819@qq.com>
2017-04-05 21:31:13 +0800

    添加AddConnection面板

---
+++ SalamanderWnmp/UI/AddRedisConnWin.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SalamanderWnmp.UI
+{
+    /// <summary>
+    /// AddRedisConnWin.xaml 的交互逻辑
+    /// </summary>
+    public partial class AddRedisConnWin : Window
+    {
+        public AddRedisConnWin()
+        {
+            InitializeComponent();
+        }
+
+        private void title_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+            e.Handled = true;
+        }
+
+        private void btn_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+            e.Handled = true;
+        }
+    }
+}
