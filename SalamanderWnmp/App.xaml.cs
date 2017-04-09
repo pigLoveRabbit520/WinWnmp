e68097d063f746482ee3a59a2fc777985aa5c139
salamander-mh <1906747819@qq.com>
2017-04-09 17:51:20 +0800

    只允许一个实例运行

--- SalamanderWnmp/App.xaml.cs
+++ SalamanderWnmp/App.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Threading;
 using System.Windows;
 
 namespace SalamanderWnmp
@@ -12,9 +13,25 @@ namespace SalamanderWnmp
     /// </summary>
     public partial class App : Application
     {
+
+        Mutex mutex;
+
         public App()
         {
+            this.Startup += new StartupEventHandler(App_Startup);
             DispatcherHelper.Initialize();
+        }
+
+        private void App_Startup(object sender, StartupEventArgs e)
+        {
+            bool ret;
+            mutex = new Mutex(true, "ElectronicNeedleTherapySystem", out ret);
+
+            if (!ret)
+            {
+                MessageBox.Show("已有一个程序实例运行");
+                Environment.Exit(0);
+            }
         }
     }
 }
