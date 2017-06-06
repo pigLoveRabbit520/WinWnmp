7ad225a0351d0eafa6e61172ef403526888373ef
salamander-mh <1906747819@qq.com>
2017-06-06 18:12:31 +0800

    初步使用AvalonEdit编辑器

---
+++ SalamanderWnmp/EditorClass/MyCompletionData.cs
@@ -0,0 +1,44 @@
+﻿
+using System;
+using ICSharpCode.AvalonEdit.CodeCompletion;
+using ICSharpCode.AvalonEdit.Document;
+using ICSharpCode.AvalonEdit.Editing;
+#if NREFACTORY
+using ICSharpCode.NRefactory.Editor;
+#endif
+
+namespace SalamanderWnmp.EditorClass
+{
+	/// <summary>
+	/// Implements AvalonEdit ICompletionData interface to provide the entries in the completion drop down.
+	/// </summary>
+	public class MyCompletionData : ICompletionData
+	{
+		public MyCompletionData(string text)
+		{
+			this.Text = text;
+		}
+		
+		public System.Windows.Media.ImageSource Image {
+			get { return null; }
+		}
+		
+		public string Text { get; private set; }
+		
+		// Use this property if you want to show a fancy UIElement in the drop down list.
+		public object Content {
+			get { return this.Text; }
+		}
+		
+		public object Description {
+			get { return "Description for " + this.Text; }
+		}
+		
+		public double Priority { get { return 0; } }
+		
+		public void Complete(TextArea textArea, ISegment completionSegment, EventArgs insertionRequestEventArgs)
+		{
+			textArea.Document.Replace(completionSegment, this.Text);
+		}
+	}
+}
--- SalamanderWnmp/UI/CodePanelWin.xaml.cs
+++ SalamanderWnmp/UI/CodePanelWin.xaml.cs
@@ -1,10 +1,21 @@
-﻿using SalamanderWnmp.Tool;
+﻿using ICSharpCode.AvalonEdit.CodeCompletion;
+using ICSharpCode.AvalonEdit.Highlighting;
+using Microsoft.Win32;
+using SalamanderWnmp.Tool;
 using SalamanderWnmp.UserClass;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.IO;
 using System.Windows;
 using System.Windows.Input;
+using ICSharpCode.AvalonEdit.Document;
+using ICSharpCode.AvalonEdit.Editing;
+using ICSharpCode.AvalonEdit.Folding;
+using System.Windows.Controls;
+using SalamanderWnmp.EditorClass;
+using System.Xml;
+using System.Windows.Threading;
 
 namespace SalamanderWnmp.UI
 {
@@ -37,8 +48,45 @@ namespace SalamanderWnmp.UI
 
         public CodePanelWin()
         {
+            // Load our custom highlighting definition
+            IHighlightingDefinition customHighlighting;
+            using (Stream s = typeof(CodePanelWin).Assembly.GetManifestResourceStream("SalamanderWnmp.CustomHighlighting.xshd"))
+            {
+                if (s == null)
+                    throw new InvalidOperationException("Could not find embedded resource");
+                using (XmlReader reader = new XmlTextReader(s))
+                {
+                    customHighlighting = ICSharpCode.AvalonEdit.Highlighting.Xshd.
+                        HighlightingLoader.Load(reader, HighlightingManager.Instance);
+                }
+            }
+            // and register it in the HighlightingManager
+            HighlightingManager.Instance.RegisterHighlighting("Custom Highlighting", new string[] { ".cool" }, customHighlighting);
+
+
             InitializeComponent();
             this.MouseMove += CodePanelWin_MouseMove;
+
+            #if DOTNET4
+			this.SetValue(TextOptions.TextFormattingModeProperty, TextFormattingMode.Display);
+            #endif
+            //propertyGridComboBox.SelectedIndex = 2;
+
+            //textEditor.TextArea.SelectionBorder = null;
+
+            //textEditor.SyntaxHighlighting = HighlightingManager.Instance.GetDefinition("C#");
+            //textEditor.SyntaxHighlighting = customHighlighting;
+            // initial highlighting now set by XAML
+
+            textEditor.TextArea.TextEntering += textEditor_TextArea_TextEntering;
+            textEditor.TextArea.TextEntered += textEditor_TextArea_TextEntered;
+
+            DispatcherTimer foldingUpdateTimer = new DispatcherTimer();
+            foldingUpdateTimer.Interval = TimeSpan.FromSeconds(2);
+            foldingUpdateTimer.Tick += delegate { UpdateFoldings(); };
+            foldingUpdateTimer.Start();
+
+
         }
 
         private void CodePanelWin_MouseMove(object sender, MouseEventArgs e)
@@ -92,7 +140,7 @@ namespace SalamanderWnmp.UI
 
         private void btnRun_Click(object sender, RoutedEventArgs e)
         {
-            string code = this.txtCode.Text;
+            string code = this.textEditor.Text;
             if(string.IsNullOrEmpty(code))
             {
                 MessageBox.Show("请输入代码");
@@ -141,16 +189,6 @@ namespace SalamanderWnmp.UI
         }
 
        
-        private void txtCode_PreviewKeyDown(object sender, KeyEventArgs e)
-        {
-            if(e.Key == Key.Tab)
-            {
-                int index = txtCode.SelectionStart;
-                //txtCode.
-            }
-            e.Handled = true;
-        }
-
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             this.lanList.DataContext = this;
@@ -161,5 +199,150 @@ namespace SalamanderWnmp.UI
             this.txtOutput.Text = "";            
         }
 
+        string currentFileName;
+
+        void openFileClick(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.CheckFileExists = true;
+            if (dlg.ShowDialog() ?? false)
+            {
+                currentFileName = dlg.FileName;
+                textEditor.Load(currentFileName);
+                textEditor.SyntaxHighlighting = HighlightingManager.Instance.GetDefinitionByExtension(Path.GetExtension(currentFileName));
+            }
+        }
+
+        void saveFileClick(object sender, EventArgs e)
+        {
+            if (currentFileName == null)
+            {
+                SaveFileDialog dlg = new SaveFileDialog();
+                dlg.DefaultExt = ".txt";
+                if (dlg.ShowDialog() ?? false)
+                {
+                    currentFileName = dlg.FileName;
+                }
+                else
+                {
+                    return;
+                }
+            }
+            textEditor.Save(currentFileName);
+        }
+
+        //void propertyGridComboBoxSelectionChanged(object sender, RoutedEventArgs e)
+        //{
+        //    if (propertyGrid == null)
+        //        return;
+        //    switch (propertyGridComboBox.SelectedIndex)
+        //    {
+        //        case 0:
+        //            propertyGrid.SelectedObject = textEditor;
+        //            break;
+        //        case 1:
+        //            propertyGrid.SelectedObject = textEditor.TextArea;
+        //            break;
+        //        case 2:
+        //            propertyGrid.SelectedObject = textEditor.Options;
+        //            break;
+        //    }
+        //}
+
+        CompletionWindow completionWindow;
+
+        void textEditor_TextArea_TextEntered(object sender, TextCompositionEventArgs e)
+        {
+            if (e.Text == ".")
+            {
+                // open code completion after the user has pressed dot:
+                completionWindow = new CompletionWindow(textEditor.TextArea);
+                // provide AvalonEdit with the data:
+                IList<ICompletionData> data = completionWindow.CompletionList.CompletionData;
+                data.Add(new MyCompletionData("Item1"));
+                data.Add(new MyCompletionData("Item2"));
+                data.Add(new MyCompletionData("Item3"));
+                data.Add(new MyCompletionData("Another item"));
+                completionWindow.Show();
+                completionWindow.Closed += delegate {
+                    completionWindow = null;
+                };
+            }
+        }
+
+        void textEditor_TextArea_TextEntering(object sender, TextCompositionEventArgs e)
+        {
+            if (e.Text.Length > 0 && completionWindow != null)
+            {
+                if (!char.IsLetterOrDigit(e.Text[0]))
+                {
+                    // Whenever a non-letter is typed while the completion window is open,
+                    // insert the currently selected element.
+                    completionWindow.CompletionList.RequestInsertion(e);
+                }
+            }
+            // do not set e.Handled=true - we still want to insert the character that was typed
+        }
+
+        #region Folding
+        FoldingManager foldingManager;
+        object foldingStrategy;
+
+        void HighlightingComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (textEditor.SyntaxHighlighting == null)
+            {
+                foldingStrategy = null;
+            }
+            else
+            {
+                switch (textEditor.SyntaxHighlighting.Name)
+                {
+                    case "XML":
+                        foldingStrategy = new XmlFoldingStrategy();
+                        textEditor.TextArea.IndentationStrategy = new ICSharpCode.AvalonEdit.Indentation.DefaultIndentationStrategy();
+                        break;
+                    case "C#":
+                    case "C++":
+                    case "PHP":
+                    case "Java":
+                        textEditor.TextArea.IndentationStrategy = new ICSharpCode.AvalonEdit.Indentation.CSharp.CSharpIndentationStrategy(textEditor.Options);
+                        foldingStrategy = new BraceFoldingStrategy();
+                        break;
+                    default:
+                        textEditor.TextArea.IndentationStrategy = new ICSharpCode.AvalonEdit.Indentation.DefaultIndentationStrategy();
+                        foldingStrategy = null;
+                        break;
+                }
+            }
+            if (foldingStrategy != null)
+            {
+                if (foldingManager == null)
+                    foldingManager = FoldingManager.Install(textEditor.TextArea);
+                UpdateFoldings();
+            }
+            else
+            {
+                if (foldingManager != null)
+                {
+                    FoldingManager.Uninstall(foldingManager);
+                    foldingManager = null;
+                }
+            }
+        }
+
+        void UpdateFoldings()
+        {
+            if (foldingStrategy is BraceFoldingStrategy)
+            {
+                ((BraceFoldingStrategy)foldingStrategy).UpdateFoldings(foldingManager, textEditor.Document);
+            }
+            if (foldingStrategy is XmlFoldingStrategy)
+            {
+                ((XmlFoldingStrategy)foldingStrategy).UpdateFoldings(foldingManager, textEditor.Document);
+            }
+        }
     }
+    #endregion
+
 }
