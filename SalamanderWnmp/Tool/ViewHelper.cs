215d8dda68a550fdf84674f522046d2853445ad9
salamander-mh <1906747819@qq.com>
2017-06-11 08:28:27 +0800

    添加MainWIndow ListBox水平滚动能力

--- SalamanderWnmp/Tool/ViewHelper.cs
+++ SalamanderWnmp/Tool/ViewHelper.cs
@@ -14,5 +14,24 @@ namespace SalamanderWnmp.Tool
         {
             return VisualTreeHelper.GetParent(view);
         }
+
+
+        public static T FindVisualChild<T>(DependencyObject obj) where T : DependencyObject
+        {
+            if (obj != null)
+            {
+                for (int i = 0; i < VisualTreeHelper.GetChildrenCount(obj); i++)
+                {
+                    DependencyObject child = VisualTreeHelper.GetChild(obj, i);
+                    if (child != null && child is T)
+                    {
+                        return (T)child;
+                    }
+                    T childItem = FindVisualChild<T>(child);
+                    if (childItem != null) return childItem;
+                }
+            }
+            return null;
+        }
     }
 }
---
+++ SalamanderWnmp/UserClass/SalamanderListBox.cs
@@ -0,0 +1,41 @@
+﻿using SalamanderWnmp.Tool;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace SalamanderWnmp.UserClass
+{
+    /// <summary>
+    /// 带平滑滚动的ListBox
+    /// </summary>
+    class SalamanderListBox: ListBox
+    {
+        public SalamanderListBox()
+        {
+            this.AddHandler(ListBox.MouseWheelEvent, new MouseWheelEventHandler(list_MouseWheel), true);
+        }
+
+
+        private void list_MouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            ItemsControl items = (ItemsControl)sender;
+            ScrollViewer scroll = ViewHelper.FindVisualChild<ScrollViewer>(items);
+            if (scroll != null)
+            {
+                int d = e.Delta;
+                if (d > 0)
+                {
+                    scroll.LineRight();
+                }
+                if (d < 0)
+                {
+                    scroll.LineLeft();
+                }
+                scroll.ScrollToTop();
+            }
+        }
+    }
+}
