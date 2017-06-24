5a7edfeeb86dd1cb144aea5ae0b59878a5bc3c8a
salamander-mh <1906747819@qq.com>
2017-06-24 10:11:43 +0800

    添加设置代理类

---
+++ SalamanderWnmp/Tool/ProxyHelper.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalamanderWnmp.Tool
+{
+    class ProxyHelper
+    {
+
+        /// <summary>
+        /// 获取正在使用的代理
+        /// </summary>
+        /// <returns></returns>
+        private static string GetProxyServer()
+        {
+            //打开注册表 
+            RegistryKey regKey = Registry.CurrentUser;
+            string SubKeyPath = "Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings";
+            RegistryKey optionKey = regKey.OpenSubKey(SubKeyPath, true);             // 更改健值，设置代理， 
+            string actualProxy = optionKey.GetValue("ProxyServer").ToString();
+            regKey.Close();
+            return actualProxy;
+        }
+
+        private static bool GetProxyStatus()
+        {
+            //打开注册表 
+            RegistryKey regKey = Registry.CurrentUser;
+            string SubKeyPath = "Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings";
+            RegistryKey optionKey = regKey.OpenSubKey(SubKeyPath, true);             // 更改健值，设置代理， 
+            int actualProxyStatus = Convert.ToInt32(optionKey.GetValue("ProxyEnable"));
+            regKey.Close();
+            return actualProxyStatus == 1 ? true : false;
+        }
+    }
+}
